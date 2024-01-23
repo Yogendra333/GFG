5df2ab5896920f1b7ea19656693f4473f3bd6d17
YOGENDRA SINGH <71462116+Yogendra333@users.noreply.github.com>
2024-01-23 19:40:20 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Course Schedule/course-schedule.py
@@ -0,0 +1,95 @@
+#User function Template for python3
+
+from collections import defaultdict
+class Solution:
+    WHITE = 1
+    GRAY = 2
+    BLACK = 3
+    def findOrder(self, n, m, prerequisites):
+        # Code here
+        numCourses = n
+        # Create the adjacency list representation of the graph
+        adj_list = defaultdict(list)
+
+        # A pair [a, b] in the input represents edge from b --> a
+        for dest, src in prerequisites:
+            adj_list[src].append(dest)
+
+        topological_sorted_order = []
+        is_possible = True
+
+        # By default all vertces are WHITE
+        color = {k: Solution.WHITE for k in range(numCourses)}
+        def dfs(node):
+            nonlocal is_possible
+
+            # Don't recurse further if we found a cycle already
+            if not is_possible:
+                return
+
+            # Start the recursion
+            color[node] = Solution.GRAY
+
+            # Traverse on neighboring vertices
+            if node in adj_list:
+                for neighbor in adj_list[node]:
+                    if color[neighbor] == Solution.WHITE:
+                        dfs(neighbor)
+                    elif color[neighbor] == Solution.GRAY:
+                         # An edge to a GRAY vertex represents a cycle
+                        is_possible = False
+
+            # Recursion ends. We mark it as black
+            color[node] = Solution.BLACK
+            topological_sorted_order.append(node)
+
+        for vertex in range(numCourses):
+            # If the node is unprocessed, then call dfs on it.
+            if color[vertex] == Solution.WHITE:
+                dfs(vertex)
+
+        return topological_sorted_order[::-1] if is_possible else []
+        
+        
+
+#{ 
+ # Driver Code Starts
+# Driver Program
+
+import sys
+sys.setrecursionlimit(10**6)
+        
+def check(graph, N, res):
+	map=[0]*N
+	for i in range(N):
+		map[res[i]]=i
+	for i in range(N):
+		for v in graph[i]:
+			if map[i] > map[v]:
+				return False
+	return True
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        n,m = list(map(int, input().strip().split()))
+        adj = [[] for i in range(n)]
+        prerequisites = []
+        
+        for i in range(m):
+            u,v=map(int,input().split())
+            adj[v].append(u)
+            prerequisites.append([u,v])
+            
+        ob = Solution()
+        
+        res = ob.findOrder(n, m, prerequisites)
+        
+        if(not len(res)):
+            print("No Ordering Possible")
+        else:
+            if check(adj, n, res):
+                print(1)
+            else:
+                print(0)
+# } Driver Code Ends
